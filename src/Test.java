bba37644eff036179c6191e2e08525dddb40b93d
Shilun LI <lishilun1993@gmail.com>
2017-12-19 12:48:53 -0500

    no message

--- src/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        String str = " \t\tq\tqsdf sdfsdf  dsf\t\tsadasd \t";
-        String[] splited = str.split("[ \t]+");
-        System.out.println(splited.length);
-        
-    }
-}
